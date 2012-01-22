fd13b1a9856059e75700d42b428e2978c2b461cd
johabu <johabu96@yahoo.de>
2012-01-22 18:27:04 +0100

    Inserted factors for overall 13 units in quantity volume

--- unitconv_units.h
+++ unitconv_units.h
@@ -4,8 +4,10 @@
 
 extern char *Quantities[];
 
-extern char *Units_short[2][12]; 
-extern char *Units_long[2][12];
+extern char *Units_short[3][13]; 
+extern char *Units_long[3][13];
+
+extern int Quantity_units_num[];
 
 extern double Length_factors[12][12]; 
 extern double Area_factors[12][12];
